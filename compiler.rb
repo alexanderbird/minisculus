20d9f3bf2a269b2b353ff03692967ecea67df1b9
Alexander Bird <alexkbird@gmail.com>
2016-01-21 18:40:20 -0700

    compiler module reads from file and prints the results

--- compiler.rb
+++ compiler.rb
@@ -1,2 +1,18 @@
-class Compiler
+module Compiler
+  def self.compile filename
+    file = File.open(filename)
+    code = ''
+    begin
+      code = file.read
+    rescue Exception => e
+      raise e
+    ensure
+      file.close
+    end
+    print code
+  end
+end
+
+if __FILE__ == $0
+  Compiler.compile ARGV.first
 end
